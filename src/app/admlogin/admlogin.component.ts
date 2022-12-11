e560d39fcfe8ecdcdcea87c2c35ab2f8abe60a66
AmalAlby <amalalbyn@gmail.com>
2022-12-11 18:16:01 +0530

    complaint app admin login created

---
+++ src/app/admlogin/admlogin.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admlogin',
+  templateUrl: './admlogin.component.html',
+  styleUrls: ['./admlogin.component.css']
+})
+export class AdmloginComponent {
+
+  constructor(private route:Router){
+    localStorage.removeItem("userInfo");
+  }
+  username = ""
+  password = ""
+  
+  readValues = ()=>{
+    if(this.username=="admin"&&this.password=="12345"){
+      alert("Logged in")
+      // this.route.navigate(["/dboard"])
+    }else{
+      alert("Invalid Credentials");
+    }
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdmloginComponent } from './admlogin/admlogin.component';
+import { AdmnavbarComponent } from './admnavbar/admnavbar.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const myroutes:Routes = [
+  {
+    path:"",
+    component:AdmloginComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdmloginComponent,
+    AdmnavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(myroutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
